97ca67a584900e69ec2bee25e04cb83108f38bab
Jaanuszek <kubex564@gmail.com>
2024-05-22 23:00:03 +0200

    VBO

--- GameEngine/GameEngine/src/main.cpp
+++ GameEngine/GameEngine/src/main.cpp
@@ -2,6 +2,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+
 int main() {
 	glfwInit();
 
@@ -20,15 +21,33 @@ int main() {
 
 	gladLoadGL();
 
+	float vertices[6] = {
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 	glViewport(0,0,800,600);
 
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glfwSwapBuffers(window);
 
+
+
 	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);
+
 		glfwPollEvents();
-		
 	}
 
 
