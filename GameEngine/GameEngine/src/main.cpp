2adc8ca96f62b85b0fd8e6838df70420aff4c3dd
Jaanuszek <kubex564@gmail.com>
2024-05-29 10:48:11 +0200

    glAttribpointer and shader

--- GameEngine/GameEngine/src/main.cpp
+++ GameEngine/GameEngine/src/main.cpp
@@ -2,13 +2,69 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+static unsigned int CompileShader(unsigned int type,const std::string& source) {
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (!result) {
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length*sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile " <<(type==GL_VERTEX_SHADER ?  "vertex" : "fragment") << " shader!"<< std::endl;
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+	
+	return program;
+}
+
+std::string vertexShader =
+	"#version 330 core\n"
+	"\n"
+	"layout(location =0) in vec4 position;\n"
+	"\n"
+	"void main()\n"
+	"{\n"
+	"	gl_Position = position;\n"
+	"}\n";
+std::string fragmentShader =
+	"#version 330 core\n"
+	"\n"
+	"layout(location =0 ) out vec4 color;\n"
+	"\n"
+	"void main()\n"
+	"{\n"
+	"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+	"}\n";
 
 int main() {
 	glfwInit();
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	/*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
 
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Game Engine", NULL, NULL);
@@ -27,10 +83,19 @@ int main() {
 		 0.5f, -0.5f
 	};
 
-	unsigned int VBO;
+	unsigned int VBO, VAO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), vertices, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void*)0);
+
+
+	unsigned int shader = CreateShader(vertexShader,fragmentShader);
+	glUseProgram(shader);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	glViewport(0,0,800,600);
 
@@ -50,7 +115,7 @@ int main() {
 		glfwPollEvents();
 	}
 
-
+	glDeleteShader(shader);
 	glfwDestroyWindow(window);
 	glfwTerminate();
 
