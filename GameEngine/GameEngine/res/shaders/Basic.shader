8a0326641d51eb0f132848bc626b87b22b48b021
Jaanuszek <kubex564@gmail.com>
2024-06-29 22:20:14 +0200

    Rendering 3d cube and piramid

--- GameEngine/GameEngine/res/shaders/Basic.shader
+++ GameEngine/GameEngine/res/shaders/Basic.shader
@@ -3,8 +3,10 @@
 
 layout(location = 0) in vec4 position;
 layout(location = 1) in vec2 texCoord;
+layout(location = 2) in vec3 color;
 
 out vec2 v_TexCoord;
+out vec3 v_Color;
 
 uniform mat4 u_MVP;
 
@@ -12,14 +14,16 @@ void main()
 {
 	gl_Position = u_MVP * position;
 	v_TexCoord = texCoord;
+	v_Color = color;
 };
 
 #shader fragment
 #version 330 core
 
-layout(location = 0) out vec4 color;
-
+layout(location = 0) out vec4 color; //to i to dziala
+//out vec4 color;
 in vec2 v_TexCoord;
+in vec3 v_Color;
 
 uniform vec4 u_Color;
 uniform sampler2D u_Texture;
@@ -27,5 +31,6 @@ uniform sampler2D u_Texture;
 void main()
 {
 	vec4 texColor = texture(u_Texture, v_TexCoord);
-	color = texColor;
+	//color = u_Color;
+	color = vec4(v_Color, 1.0);
 };
--- GameEngine/GameEngine/src/main.cpp
+++ GameEngine/GameEngine/src/main.cpp
@@ -20,6 +20,72 @@
 #include "Shader.h"
 #include "Texture.h"
 
+#include "tests/TestClearColor.h"
+
+
+enum class RenderObject {
+	Cube,
+	Pyramid
+};
+
+void SetupRenderObjects(RenderObject object, VAO& vao, VBO& vbo, VBL& layout, EBO& ebo);
+
+
+float verticesCube[] = {
+	-0.5f,-0.5f,-0.5f, 0.0f,0.0f, 1.0f,0.0f,0.0f, //left bottom back 0
+	0.5f, -0.5f,-0.5f, 1.0f,0.0f, 0.0f,1.0f,0.0f, //right bottom back 1
+	0.5f,  0.5f,-0.5f, 1.0f,1.0f, 0.0f,0.0f,1.0f, //right top back 2
+	-0.5f, 0.5f,-0.5f, 0.0f,1.0f, 0.5f,0.5f,0.5f, //left top back 3
+
+	-0.5f,-0.5f,0.5f, 0.0f,0.0f, 0.2f,0.5f,0.7f, //left bottom front 4
+	0.5f, -0.5f,0.5f, 1.0f,0.0f, 1.0f,1.0f,0.0f, //right bottom front 5
+	0.5f,  0.5f,0.5f, 1.0f,1.0f, 0.0f,1.0f,1.0f, //right top front	6
+	-0.5f, 0.5f,0.5f, 0.0f,1.0f, 0.7f,0.5f,0.2f //left top front	7
+};
+
+unsigned int indicesCube[] = {
+	//back wall
+	0,1,2,
+	2,3,0,
+	//front wall
+	4,5,6,
+	6,7,4,
+	//left wall
+	0,3,7,
+	7,4,0,
+	//right wall
+	1,2,6,
+	6,5,1,
+	//bottom wall
+	0,1,5,
+	5,4,0,
+	//top wall
+	3,2,6,
+	6,7,3
+};
+
+float verticesPyramid[] = {
+	//adding colors as layout 2
+	-0.5f,-0.5f,-0.5f,0.0f,0.0f, 1.0f,0.0f,0.0f, //left bottom back 0
+	0.5f,-0.5f,-0.5f,1.0f,0.0f, 0.0f, 1.0f, 0.0f, //right bottom back 1
+	0.5f,0.5f,-0.5f,1.0f,1.0f, 0.0f, 0.0f,1.0f, //right top back 2
+	-0.5f,0.5f,-0.5f,0.0f,1.0f, 0.5f,0.5f,0.5f, //left top back 3
+
+	0.0f,0.0f,0.5f,0.5f,0.5f, 1.0f,1.0f,0.0f //top of the pyramid 4
+};
+unsigned int indicesPyramid[] = {
+	//back wall
+	0,1,2,
+	2,3,0,
+	//left wall
+	0,3,4,
+	//right wall
+	1,2,4,
+	//front wall
+	0,1,4,
+	2,3,4
+};
+
 int main() {
 	if (!glfwInit())
 		return -1;
@@ -41,10 +107,10 @@ int main() {
 	gladLoadGL();
 	{
 		float vertices[] = {
-			-50.0f, -50.0f, 0.0f, 0.0f,
-			50.0f, -50.0f, 1.0f, 0.0f, 
-			50.0f, 50.0f, 1.0f, 1.0f, 
-			-50.0f, 50.0f, 0.0f, 1.0f
+			-0.5f, -0.5f, 0.0f, 0.0f,
+			0.5f, -0.5f, 1.0f, 0.0f, 
+			0.5f, 0.5f, 1.0f, 1.0f, 
+			-0.5f, 0.5f, 0.0f, 1.0f
 		};
 
 		unsigned int indices[] = {
@@ -52,36 +118,11 @@ int main() {
 			2,3,0
 		};
 
+
+
 		GLCall(glEnable(GL_BLEND));
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-		VAO vao;
-		VBO vbo(vertices, 4*4 * sizeof(float));
-
-		VBL layout;
-		layout.Push(GL_FLOAT,2);
-		layout.Push(GL_FLOAT, 2);
-		vao.AddBuffer(vbo, layout);
-
-		EBO ebo(indices, 6);
-
-		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-
-		Shader shader("res/shaders/Basic.shader");
-		shader.Bind();
-		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-
-		Texture texture("res/textures/pudzilla.png");
-		texture.Bind();
-		shader.SetUniform1i("u_Texture", 0);
-
-		vao.Unbind();
-		vbo.Unbind();
-		ebo.Unbind();
-		shader.Unbind();
-
-		Renderer renderer;
+		GLCall(glEnable(GL_DEPTH_TEST));
 
 		IMGUI_CHECKVERSION();
 		ImGui::CreateContext();
@@ -89,12 +130,73 @@ int main() {
 		ImGui::StyleColorsDark();
 		ImGui_ImplOpenGL3_Init((char*)glGetString(330));
 
+		VAO vao1;
+		//cube
+		VBO vbo1(verticesCube, 8 * 8 * sizeof(float));
+		VBL layout1;
+		layout1.Push(GL_FLOAT, 3);
+		layout1.Push(GL_FLOAT, 2);
+		layout1.Push(GL_FLOAT, 3);
+		vao1.AddBuffer(vbo1, layout1);
+		EBO ebo1(indicesCube, 36);
+
+		VAO vao2;
+		//pyramid
+		VBO vbo2(verticesPyramid, 5 * 8 * sizeof(float));
+		VBL layout2;
+		layout2.Push(GL_FLOAT, 3);
+		layout2.Push(GL_FLOAT, 2);
+		layout2.Push(GL_FLOAT, 3);
+		vao2.AddBuffer(vbo2, layout2);
+		EBO ebo2(indicesPyramid, 18);
+
+		//glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+		//glm::perspective(fov, aspect, near, far)
+		//ascpet = width/height
+		//near  - how close to the camera
+		//far - how far from the camera
+		//glm::mat4 proj = glm::perspective(glm::radians(45.0f), 960.0f / 540.0f, 0.1f, 100.0f); // first step
+		//moving object slightly backwards in the scene
+		//if we want to move object to the right we need to move the camera to the left if backwards to the front and so on
+		//
+		//glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f)); // third step
+
+		Shader shader1("res/shaders/Basic.shader");
+		shader1.Bind();
+		shader1.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+		Texture texture1("res/textures/pudzilla.png");
+		texture1.Bind();
+		shader1.SetUniform1i("u_Texture", 0);
+
+		Shader shader2("res/shaders/Basic.shader");
+		shader2.Bind();
+		shader2.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+		Texture texture2("res/textures/pudzilla.png");
+		texture2.Bind();
+		shader2.SetUniform1i("u_Texture", 0);
+
+		vao1.Unbind();
+		vao2.Unbind();
+		vbo1.Unbind();
+		vbo2.Unbind();
+		ebo1.Unbind();
+		ebo2.Unbind();
+		shader1.Unbind();
+		shader2.Unbind();
+
+		Renderer renderer;
 
 		float r = 0.0f;
 		float increment = 0.01f;
 
-		glm::vec3 translationA(200, 200, 0);
-		glm::vec3 translationB(400, 200, 0);
+		glm::vec3 translationA(0.0f, 0.0f, 0.0f);
+		glm::vec3 viewTranslation(0.0f, 0.0f, -3.0f);
+		float angle = 0.0f;
+
+		RenderObject renderObject = RenderObject::Cube;
+		bool plsWork = true;
 
 		while (!glfwWindowShouldClose(window)) {
 			renderer.Clear();
@@ -102,38 +204,40 @@ int main() {
 			ImGui_ImplOpenGL3_NewFrame();
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
+			{
+				glm::mat4 proj = glm::perspective(glm::radians(45.0f), 960.0f / 540.0f, 0.1f, 100.0f);
+				glm::mat4 view = glm::translate(glm::mat4(1.0f), viewTranslation);
+				glm::mat4 model = glm::mat4(1.0f); //model is a second step according to book xd
+				model = glm::rotate(model, glm::radians(angle), glm::vec3(0.5f, 1.0f, 0.0f));
+				model = glm::translate(model, translationA);
 
+				glm::mat4 mvp = proj * view * model; // mvp - model view projection, due to matrix multiplication is reversed from (right to left)
+				if (renderObject == RenderObject::Cube) {
+					shader1.Bind();
+					shader1.SetUniformMat4f("u_MVP", mvp);
+					renderer.Draw(vao1, ebo1, shader1);
+				}
+				else if (renderObject == RenderObject::Pyramid) {
+					shader2.Bind();
+					shader2.SetUniformMat4f("u_MVP", mvp);
+					renderer.Draw(vao2, ebo2, shader2);
+				}
+			}
 
 			{
-				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-				glm::mat4 mvp = proj * view * model;
-				shader.Bind();
-				shader.SetUniformMat4f("u_MVP", mvp);
-
-				renderer.Draw(vao, ebo, shader);
+			ImGui::Begin("Jabol");
+			if(ImGui::Button("Render Cube")) {
+				renderObject = RenderObject::Cube;
 			}
-			//shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-
-			{
-				glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-				glm::mat4 mvp = proj * view * model;
-				shader.Bind();
-				shader.SetUniformMat4f("u_MVP", mvp);
-
-				renderer.Draw(vao, ebo, shader);
+			ImGui::SameLine();
+			if(ImGui::Button("Render Pyramid")) {
+				renderObject = RenderObject::Pyramid;
 			}
-
-			if (r > 1.0f) {
-				increment = -0.01f;
-			}
-			else if (r < 0.0f) {
-				increment = 0.01f;
-			}
-			r += increment;
-			{
-			ImGui::Begin("Hello, world!");
-			ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);  
-			ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
+			ImGui::SliderFloat3("Translation A", &translationA.x, -1.0f, 1.0f);  
+			ImGui::SliderFloat("View Translation A x", &viewTranslation.x, -1.0f, 1.0f);
+			ImGui::SliderFloat("View Translation A y", &viewTranslation.y, -1.0f, 1.0f);
+			ImGui::SliderFloat("View Translation A z", &viewTranslation.z, -10.0f, 10.0f);
+			ImGui::SliderFloat("Angle", &angle, 0.0f, 360.0f);
 			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 			ImGui::End();
 			}
@@ -154,4 +258,26 @@ int main() {
 	glfwTerminate();
 
 	return 0;
+}
+
+void SetupRenderObjects(RenderObject object, VAO& vao, VBO& vbo, VBL& layout, EBO& ebo) {
+	//vao.Bind();
+	if (object == RenderObject::Cube) {
+		VBO vbo(verticesCube, 8 * 8 * sizeof(float));
+		VBL layout;
+		layout.Push(GL_FLOAT, 3);
+		layout.Push(GL_FLOAT, 2);
+		layout.Push(GL_FLOAT, 3);
+		vao.AddBuffer(vbo, layout);
+		EBO ebo(indicesCube, 36);
+	}
+	else if (object == RenderObject::Pyramid) {
+		VBO vbo(verticesPyramid, 5 * 8 * sizeof(float));
+		VBL layout;
+		layout.Push(GL_FLOAT, 3);
+		layout.Push(GL_FLOAT, 2);
+		layout.Push(GL_FLOAT, 3);
+		vao.AddBuffer(vbo, layout);
+		EBO ebo(indicesPyramid, 18);
+	}
 }
